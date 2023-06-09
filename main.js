f05b9f051427fdde6571b52634417b1ac09747d5
CherepokBear <sibgufk684@gmail.com>
2023-06-09 22:25:14 +0300

    09.06.22.25

--- main.js
+++ main.js
@@ -66,4 +66,90 @@ const reply = () => {
   }
 }
 
+buttonElement.addEventListener('click', () => {
+  const oldListElement = listElement.innerHTML;
+
+  const startAt = Date.now();
+
+  buttonElement.disabled = true;
+  buttonElement.textContent = "Элемент добавлятся...";
+
+  fetch('https://wedev-api.sky.pro/api/v1/dima-vorobev/comments',
+    {
+      method: 'POST',
+      body: JSON.stringify(
+        {
+          text: textInputElement.value,
+          name: nameInputElement.value,
+        })
+    })
+
+    .then((response) => {
+      if (response.status === 201) {
+        return response.json();
+      } else {
+        return Promise.reject("Сервер упал")
+      }
+    })
+    .then((response) => {
+      return response;
+    })
+    .then((responseData) => {
+      return fetch("https://wedev-api.sky.pro/api/v1/dima-vorobev/comments", {
+        method: "GET",
+      });
+    })
+    .then((response) => {
+      return response;
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      return response;
+    })
+    .then((responseData) => {
+      const ret = responseData.comments.map((comment, index) => {
+        const date = new Date(comment.date)
+        return {
+          name: comment.author.name,
+          date: date.getDate().toString().padStart(2, '0') + '.' +
+            (date.getMonth() + 1).toString().padStart(2, '0') + '.' +
+            date.getFullYear().toString().slice(-2) + ' ' +
+            date.getHours().toString().padStart(2, '0') + ':' +
+            date.getMinutes().toString().padStart(2, '0'),
+          coments: comment.text,
+          likes: comment.likes,
+          isActiveLike: false,
+        }
+      });
+      numberLikes = ret;
+      nameInputElement.value = "";
+      textInputElement.value = "";
+      renderComments();
+    })
+    // .then((response) => {
+    //   return response;
+    // })
+    // .then((responseData) => {
+    //   if (response.status === 500) {
+    //     throw new Error("Ошибка сервера")
+    //   } else {
+    //     return response.json();
+    //   }
+
+    // })
+    .then((data) => {
+      buttonElement.disabled = false;
+      buttonElement.textContent = "Написать";
+    })
+    .catch((error) => {
+      buttonElement.disabled = false;
+      buttonElement.textContent = "Написать";
+
+      alert('Имя и комментарий должн быть не менее трех символов!');
+      console.log(error)
+    })
+});
+
 
